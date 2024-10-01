fcab3fabb33cc8006bbe5394205c15fb5edfe5c5
Indro Lie <indro@vouch.sg>
2024-10-01 21:30:57 +0700

    [feat] limit tags to top 10 most popular

--- src/redux/saga/api/api.tag.js
+++ src/redux/saga/api/api.tag.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const fetchTagsApi = () => {
     return axios.get('https://api.stackexchange.com/2.3/tags', {
         params: {
+            pagesize: 10, // limit to 10 most popular tags
             order: 'desc',
             sort: 'popular',
             filter: 'default',
